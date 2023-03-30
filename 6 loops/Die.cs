fca3e1bd9a90a1e135c29bfb95af6bdbfd1aaacb
Jila Phonprasith <jilaphonprasith@gmail.com>
2023-03-30 10:52:29 -0400

    Add project files.

---
+++ 6 loops/Die.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+        public Die(int roll)
+        {
+            _generator = new Random();
+            if (roll < 1)
+                _roll = 1;
+            else if (roll > 6)
+                _roll = 6;
+            else
+                _roll = roll;
+        }
+
+        public int Roll { get { return _roll; } }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+
+        }
+
+        public void DrawRoll()
+        {
+            switch(_roll)
+            {
+                case 1:
+                    Console.WriteLine(" ---------");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine("|    O    |");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine(" ---------");
+                    break;
+
+                case 2:
+                    Console.WriteLine(" ---------");
+                    Console.WriteLine("|  O      |");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine("|      O  |");
+                    Console.WriteLine(" ---------");
+                    break;
+
+                case 3:
+                    Console.WriteLine(" ---------");
+                    Console.WriteLine("|  O      |");
+                    Console.WriteLine("|    O    |");
+                    Console.WriteLine("|      O  |");
+                    Console.WriteLine(" ---------");
+                    break;
+
+                case 4:
+                    Console.WriteLine(" ---------");
+                    Console.WriteLine("|  O   O  |");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine("|  O   O  |");
+                    Console.WriteLine(" ---------");
+                    break;
+
+                case 5:
+                    Console.WriteLine(" ---------");
+                    Console.WriteLine("|  O   O  |");
+                    Console.WriteLine("|    O    |");
+                    Console.WriteLine("|  O   O  |");
+                    Console.WriteLine(" ---------");
+                    break;
+
+                case 6:
+                    Console.WriteLine(" ---------");
+                    Console.WriteLine("|  O   O  |");
+                    Console.WriteLine("|  O   O  |");
+                    Console.WriteLine("|  O   O  |");
+                    Console.WriteLine(" ---------");
+                    break;
+
+
+
+
+            }
+        }
+
+
+
+       
+    }
+
+}
+   
+    
+
+  
---
+++ 6 loops/Program.cs
@@ -0,0 +1,184 @@
+﻿using System.Threading;
+using System.Transactions;
+
+namespace _6_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string choice = "";
+
+            //while (choice != "q")
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("-- Menu --");
+            //    Console.WriteLine();
+            //    Console.WriteLine("1 - Prompter");
+            //    Console.WriteLine("2 - Percent Passing");
+            //    Console.WriteLine("3 - Odd Sum");
+            //    Console.WriteLine("4 - Dice Game");
+            //    Console.WriteLine("5 - Random Numbers");
+            //    Console.WriteLine("...");
+            //    Console.WriteLine("x - Menu Option x");
+            //    Console.WriteLine("Q - Quit");
+            //    Console.WriteLine();
+            //    choice = Console.ReadLine().ToLower().Trim();
+            //    Console.WriteLine();
+
+            //    if (choice == "1")
+            //    {
+            //        Console.WriteLine("You chose option 1");
+            //        Console.WriteLine("Hit ENTER to continue.");
+            //        Console.ReadLine();
+            //        RunPrompter();
+
+            //    }
+            //    else if (choice == "2")
+            //    {
+            //        Console.WriteLine("You chose option 2");
+            //        Console.WriteLine("Hit ENTER to continue.");
+            //        Console.ReadLine();
+            //    }
+            //    else if (choice == "3")
+            //    {
+            //        Console.WriteLine("You chose option 3");
+            //        Console.WriteLine("Hit ENTER to continue.");
+            //        Console.ReadLine();
+            //    }
+            //    else if (choice == "4")
+            //    {
+            //        Console.WriteLine("You chose option 4");
+            //        Console.WriteLine("Hit ENTER to continue.");
+            //        Console.ReadLine();
+            //    }
+            //    else if (choice == "5")
+            //    {
+            //        Console.WriteLine("You chose option 5");
+            //        Console.WriteLine("Hit ENTER to continue.");
+            //        Console.ReadLine();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid choice, press ENTER to continue.");
+            //        Console.ReadLine();
+            //    }
+            //}
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("-- Menu --");
+                Console.WriteLine("1. Prompter");
+                Console.WriteLine("2. Percent Passing");
+                Console.WriteLine("3. Odd Sum");
+                Console.WriteLine("4. Dice Game");
+                Console.WriteLine("5. Random Numbers");
+
+                Console.WriteLine("Choose what program to run (1-5): ");
+                string choice = Console.ReadLine();
+
+                //if (choice < 1 || choice > 4)
+                //{
+                //    Console.ForegroundColor = ConsoleColor.Red;
+                //    Console.WriteLine("Invaild Outcome. Choose from choices (1-4).");
+                //    Console.WriteLine();
+                //    continue;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Invalid choice, press ENTER to continue.");
+                //    Console.ReadLine();
+                //}
+
+                switch (choice)
+                {
+                    case "1":
+                        RunPrompter();
+                        break;
+
+                    case "2":
+              
+                        RunPercentPassing();
+                        break;
+
+                    case "3":
+                        RunOddSum();
+                        break;
+
+                    case "4":
+                        RunDiceGame();
+                        break;
+
+                    case "5":
+                        RunRandomNumbers();
+                        break;
+
+
+                    default:
+                        Console.WriteLine();
+                        break;
+
+                }
+            }
+
+        }
+        public static void RunPrompter()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Prompter Program");
+
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine();
+            }
+            
+
+        }
+        public static void RunPercentPassing()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Percent Passing Program");
+                
+
+        }
+
+        public static void RunOddSum()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Odd Sum Program");
+
+        }
+
+        public static void RunDiceGame()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Dice Game Program");
+            RunDiceGame();
+        }
+
+
+        public static void RunRandomNumbers()
+        {
+            Random generator = new Random();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Random Numbers Program");
+
+            string min;
+            Console.WriteLine("Enter minimum value");
+            min = Console.ReadLine();
+
+            string max;
+            Console.WriteLine("Enter maximum value");
+            max = Console.ReadLine();
+
+
+        }
+     
+    }
+}
